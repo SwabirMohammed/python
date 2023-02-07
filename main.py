8aaac6eb1586e922d285b730946aab999ebd19d0
eMobilis <swabir49@gmail.com>
2023-02-07 08:06:31 +0300

    today

---
+++ main.py
@@ -0,0 +1,11 @@
+num1 = 400
+num2 = 600
+
+result = num1 + num2
+print(result)
+
+num3 = 500
+num4 = 10
+
+result = num3 * num4
+print(result)
---
+++ today.py
@@ -0,0 +1,15 @@
+school = "eMobilis"
+print(school)
+
+city = "Nairobi"
+print(city)
+
+country = "Kenya"
+print(country)
+
+x = "Hello"
+y = "world"
+print(x+y)
+
+
+
