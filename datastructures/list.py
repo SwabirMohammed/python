1f27fcc8377d0cc682e878adac6d11b5374e5eca
Swabir <swabir49@gmail.com>
2023-02-08 10:06:16 +0300

    list

---
+++ datastructures/list.py
@@ -0,0 +1,9 @@
+#use squared brackets for list
+# you can add or remove elements
+#it follows an order
+mylist = ["Shadya", "Swabir", "Omar", "Suheil"]
+mylist.append("Nassir",)
+mylist.append("Abdul")
+print(mylist)
+mylist.remove("Omar")
+print(mylist)
