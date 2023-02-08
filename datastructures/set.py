63a988243d8bf53cb84bcb3d29c6ba1bc7ebb5e6
Swabir <swabir49@gmail.com>
2023-02-08 10:18:53 +0300

    set

---
+++ datastructures/set.py
@@ -0,0 +1,9 @@
+#you cannot have duplicates
+#order is not guaranteed
+#access elements by their indexes
+#we use curly brackets in set
+
+myset = {"Ralph","Reagan","Alcott","Channel"}
+myset.add("Yvette")
+myset.add("Armani")
+print(myset)
---
+++ datastructures/turples.py
@@ -0,0 +1,8 @@
+#you cannot modify a tuple
+#we use normal brackets
+
+myturple = ("Yao", "James", "Ming", "Lin")
+print(myturple[0])
+print(myturple[3])
+print(myturple[1])
+print(myturple[2])
