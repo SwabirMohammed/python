f0a2cda23da6efa071aaee75f79e7fc4a043c0a9
Swabir <swabir49@gmail.com>
2023-02-09 09:14:48 +0300

    conditions

---
+++ conditions/ifstatement.py
@@ -0,0 +1,16 @@
+dial = int(input("Enter a Number"))
+if dial == 1:
+    print("Enroll for Bonga Points")
+elif dial == 2:
+    print("Enroll for M-Shwari")
+elif dial == 3:
+    print("Enroll for M-Kopa")
+elif dial == 4:
+    print("Enroll for Fuliza")
+elif dial == 5:
+    print("Activate M-Pesa")
+elif dial == 6:
+    print("Enroll for Okoa Jahazi")
+else:
+    print("Have a nice day <3")
+
