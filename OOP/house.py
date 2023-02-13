5bdc5a5fa9e3571a2022772478a2a15991241f68
Swabir <swabir49@gmail.com>
2023-02-13 09:58:48 +0300

    house

---
+++ OOP/house.py
@@ -0,0 +1,9 @@
+#Start with a capital letter
+class House1():
+    doors = 4
+    windows = 16
+    kitchen = 1
+    dinning = 2
+
+house1obj = House1()
+print(house1obj.doors, house1obj.dinning, house1obj.kitchen, house1obj.windows)
